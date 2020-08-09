a6728c0bbc9645dc7233597b5e0eaed7f9856357
cowankc <cowankc92@live.com>
2020-08-09 13:13:10 -0500

    added styling to name page

--- docs/assets/js/name.js
+++ docs/assets/js/name.js
@@ -1,76 +1,77 @@
-//variables and arrays
-const playerList = ["leopold"]
-const counter = 8; 
-let wins = 0;
-let stop = false; 
-let userChoice = []; 
-let chosenWord = []; 
-let compChoice; 
-let guess; 
+$( document ).ready(function() {
+    //variables and arrays
+    const playerList = ["leopold"]
+    const counter = 8; 
+    let wins = 0;
+    let stop = false; 
+    let userChoice = []; 
+    let chosenWord = []; 
+    let compChoice; 
+    let guess; 
 
-//resets variables and arrays back to original state 
-function restart() {
-    guess = counter
-    stop = false
-    userChoice = []
-    chosenWord = []
+    //resets variables and arrays back to original state 
+    function restart() {
+        guess = counter
+        stop = false
+        userChoice = []
+        chosenWord = []
 
-    compChoice = playerList[0].toLowerCase()
-    for (var i=0; i < compChoice.length; i++){
-        if (compChoice[i] === " ") {
-            chosenWord.push(" ")
-        } else {
-            chosenWord.push("_")
-        }
-    }
-    updateScore ()
-}
-
-// assign values to the scores
-function updateScore () {
-    document.getElementById("chosenName").innerHTML = chosenWord.join(" ")
-    document.getElementById("guesses").innerText = guess
-    document.getElementById("usedletter").innerHTML =  userChoice.join("")
-}
-
-restart()
-
-document.onkeypress = function(event) { 
-    if (stop = true) {
-        checkForLetter(event.key.toLowerCase())
-    }
-}
-
-// Game loop 
-function checkForLetter(letter) {
-    let correctLetter = false
-    for (var i=0; i<compChoice.length; i++) {
-        if (letter === compChoice[i]) {
-            chosenWord[i] = letter
-            correctLetter = true
-            if (chosenWord.join("") === compChoice) {
-                 wins++
-                 stop = true
-                 updateScore ()
-                setTimeout(restart,1000)
+        compChoice = playerList[0].toLowerCase()
+        for (var i=0; i < compChoice.length; i++){
+            if (compChoice[i] === " ") {
+                chosenWord.push(" ")
+            } else {
+                chosenWord.push("_")
             }
         }
+        updateScore ()
     }
-//checks if letter typed has already been used 
-if (!correctLetter) {
-    if (!userChoice.includes(letter)) {
-        userChoice.push(letter)
-        guess--
+
+    // assign values to the scores
+    function updateScore () {
+        document.getElementById("chosenName").innerHTML = chosenWord.join(" ")
+        document.getElementById("guesses").innerText = guess
+        document.getElementById("usedletter").innerHTML =  userChoice.join("")
     }
-    if (guess === 0) {
-        chosenWord = compChoice.split()
-        stop = true
-        setTimeout(restart, 1000)
-        alert("You missed that one!")
+
+    restart()
+
+    document.onkeypress = function(event) { 
+        if (stop = true) {
+            checkForLetter(event.key.toLowerCase())
+        }
     }
-}
 
-updateScore ()
+    // Game loop 
+    function checkForLetter(letter) {
+        let correctLetter = false
+        for (var i=0; i<compChoice.length; i++) {
+            if (letter === compChoice[i]) {
+                chosenWord[i] = letter
+                correctLetter = true
+                if (chosenWord.join("") === compChoice) {
+                    wins++
+                    stop = true
+                    updateScore ()
+                    setTimeout(restart,1000)
+                }
+            }
+        }
+    //checks if letter typed has already been used 
+    if (!correctLetter) {
+        if (!userChoice.includes(letter)) {
+            userChoice.push(letter)
+            guess--
+        }
+        if (guess === 0) {
+            chosenWord = compChoice.split()
+            stop = true
+            setTimeout(restart, 1000)
+            alert("Looks Like you'll Have too wait!")
+        }
+    }
 
-}
+    updateScore ()
 
+    }
+});
